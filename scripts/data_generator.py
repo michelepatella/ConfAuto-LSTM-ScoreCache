4c23d533ef556faf2d75dc6b5357bf327003bbb9
michelepatella <patellamichele2002@gmail.com>
2025-05-09 12:13:09 +0200

    data_generator configured to generate a single (static or dynamic) dataset

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -31,41 +31,6 @@ def create_dataset(timestamps, requests, file_name):
     # convert the dataframe to CSV file
     df.to_csv(file_name, index=False)
 
-def split_and_create_datasets(timestamps, requests, train_path, val_path, test_path):
-    """
-    Method to split and create the datasets (Training, Validation, and Testing).
-    :param timestamps: List of timestamps (first column)
-    :param requests: List of keys requested (second column)
-    :param train_path: Training dataset path
-    :param val_path: Validation dataset path
-    :param test_path: Test dataset path
-    :return:
-    """
-    # load data configuration
-    config = load_config()['data']
-    train_ratio = config['train_ratio']
-    val_ratio = config['val_ratio']
-
-    # get the tot. number of requests
-    n_requests = len(requests)
-
-    train_end = int(n_requests * train_ratio)
-    val_end = train_end + int(n_requests * val_ratio)
-
-    # slice data
-    train_requests = requests[:train_end]
-    val_requests = requests[train_end:val_end]
-    test_requests = requests[val_end:]
-
-    train_timestamps = timestamps[:train_end]
-    val_timestamps = timestamps[train_end:val_end]
-    test_timestamps = timestamps[val_end:]
-
-    # create the datasets
-    create_dataset(train_timestamps, train_requests, train_path)
-    create_dataset(val_timestamps, val_requests, val_path)
-    create_dataset(test_timestamps, test_requests, test_path)
-
 def generate_zipf_dataset(distribution_type):
     """
     Method to generate the (static or dynamic) data for the dataset.
@@ -93,14 +58,8 @@ def generate_zipf_dataset(distribution_type):
         requests = np.random.choice(keys, size=n_requests, p=probabilities)
         timestamps = np.arange(n_requests)
 
-        # split and create the static dataset
-        split_and_create_datasets(
-            timestamps,
-            requests,
-            config['static_train_path'],
-            config['static_val_path'],
-            config['static_test_path']
-        )
+        # create the static dataset
+        create_dataset(timestamps, requests, config['static_dataset_path'])
 
     # generate a dynamic distribution
     elif distribution_type == "dynamic":
@@ -132,13 +91,8 @@ def generate_zipf_dataset(distribution_type):
             requests.extend(reqs)
             timestamps.extend(np.arange(t * time_step_duration, (t + 1) * time_step_duration))
 
-        # split and create the dynamic dataset
-        split_and_create_datasets(
-            timestamps,
-            requests,
-            config['dynamic_train_path'],
-            config['dynamic_val_path'],
-            config['dynamic_test_path']
-        )
+        # create the dynamic dataset
+        create_dataset(timestamps, requests, config['dynamic_dataset_path'])
 
+generate_zipf_dataset("static")
 generate_zipf_dataset("dynamic")
