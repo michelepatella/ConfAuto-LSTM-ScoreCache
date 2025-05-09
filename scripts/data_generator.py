56e1589b46f1629542914f5ade26e82c832ec20c
michelepatella <patellamichele2002@gmail.com>
2025-05-09 13:15:34 +0200

    data generator's modularity improved further more

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -2,20 +2,20 @@ import pandas as pd
 import numpy as np
 from utils.config_loader import load_config
 
-def zipf_distribution(keys, alpha):
+def calculate_zipf_distribution_probs(keys, alpha):
     """
-    Method to generate Zipf distribution's probabilities.
+    Method to calculate the Zipf distribution's probabilities.
     :param keys: List of keys.
     :param alpha: Zipf distribution's parameter.
     :return: Zipf distribution's probabilities as output.
     """
-    # calculate the probability of the keys according to Zipf's distribution
-    probabilities = 1.0 / np.power(keys, alpha)
+    # calculate the probability of the keys according to the Zipf's distribution
+    probs = 1.0 / np.power(keys, alpha)
 
     # normalize probabilities to make sum to 1
-    probabilities = probabilities / np.sum(probabilities)
+    probs = probs / np.sum(probs)
 
-    return probabilities
+    return probs
 
 def create_dataset(timestamps, requests, file_name):
     """
@@ -31,46 +31,45 @@ def create_dataset(timestamps, requests, file_name):
     # convert the dataframe to CSV file
     df.to_csv(file_name, index=False)
 
-def generate_requests_and_timestamps(n_requests, n_keys, alpha, time_steps=None):
+def generate_static_requests(n_requests, n_keys, alpha):
     """
-    Method to generate (static or dynamic) requests and timestamps.
+    Method to generate static requests (with the associated timestamps).
     :param n_requests: Total number of requests.
     :param n_keys: Total number of keys.
-    :param alpha: Zipf distribution's parameter(s).
-    :param time_steps: Total number of time steps.
-    :return: Requests and timestamps generated as output.
+    :param alpha: Zipf distribution's parameter.
+    :return: Static requests and timestamps as output.
     """
+    # calculate the probabilities
+    probs = calculate_zipf_distribution_probs(np.arange(1, n_keys + 1), alpha)
+
+    # generate requests and timestamps
+    requests = np.random.choice(np.arange(1, n_keys + 1), size=n_requests, p=probs)
+    timestamps = np.arange(n_requests)
+
+    return requests, timestamps
+
+def generate_dynamic_requests(n_requests, n_keys, alpha_values, time_steps):
+    """
+    Method to generate dynamic requests (with the associated timestamps).
+    :param n_requests: Total number of requests.
+    :param n_keys: Total number of keys.
+    :param alpha_values: Zipf distribution's parameter values.
+    :param time_steps: Total number of time steps.
+    :return: Dynamic requests and timestamps as output.
+    """
+    # calculate the time step duration
+    time_step_duration = n_requests // time_steps
     requests, timestamps = [], []
 
-    # static requests and timestamps
-    if time_steps is None:
-
-        # take all the Zipf's distribution parameter values
-        alpha = alpha
-
-        # calculate probabilities
-        probabilities = zipf_distribution(np.arange(1, n_keys + 1), alpha)
+    # for each alpha value
+    for t, alpha in enumerate(alpha_values):
+        # calculate the probabilities
+        probs = calculate_zipf_distribution_probs(np.arange(1, n_keys + 1), alpha)
 
         # generate requests and timestamps
-        requests = np.random.choice(np.arange(1, n_keys + 1), size=n_requests, p=probabilities)
-        timestamps = np.arange(n_requests)
-
-    # dynamic requests and timestamps
-    else:
-
-        # calculate the time step duration
-        time_step_duration = n_requests // time_steps
-
-        # for each alpha value
-        for t, alpha in enumerate(alpha):
-
-            # calculate the probabilities
-            probabilities = zipf_distribution(np.arange(1, n_keys + 1), alpha)
-
-            # generate requests and timestamps
-            reqs = np.random.choice(np.arange(1, n_keys + 1), size=time_step_duration, p=probabilities)
-            requests.extend(reqs)
-            timestamps.extend(np.arange(t * time_step_duration, (t + 1) * time_step_duration))
+        reqs = np.random.choice(np.arange(1, n_keys + 1), size=time_step_duration, p=probs)
+        requests.extend(reqs)
+        timestamps.extend(np.arange(t * time_step_duration, (t + 1) * time_step_duration))
 
     return requests, timestamps
 
@@ -89,7 +88,7 @@ def generate_static_data():
     alpha = data_config['alpha']
 
     # generate static requests and timestamps
-    requests, timestamps = generate_requests_and_timestamps(n_requests, n_keys, alpha)
+    requests, timestamps = generate_static_requests(n_requests, n_keys, alpha)
 
     # create the static dataset
     create_dataset(timestamps, requests, data_config['static_dataset_path'])
@@ -116,7 +115,7 @@ def generate_dynamic_data():
     alpha_values = np.linspace(alpha_start, alpha_end, time_steps)
 
     # generate dynamic requests and timestamps
-    requests, timestamps = generate_requests_and_timestamps(n_requests, n_keys, alpha_values, time_steps)
+    requests, timestamps = generate_dynamic_requests(n_requests, n_keys, alpha_values, time_steps)
 
     # create the dynamic dataset
     create_dataset(timestamps, requests, data_config['dynamic_dataset_path'])
@@ -137,7 +136,7 @@ def generate_zipf_dataset(distribution_type):
 
     # handle errors
     else:
-        ValueError("Unknown distribution type.")
+        raise ValueError("Unknown distribution type.")
 
 generate_zipf_dataset("static")
 generate_zipf_dataset("dynamic")
