3abb36c18cdbcb481412f3c84b1019a51f508aab
michelepatella <patellamichele2002@gmail.com>
2025-05-09 13:35:37 +0200

    some code improvements

--- utils/access_log_dataset.py
+++ utils/access_log_dataset.py
@@ -8,13 +8,13 @@ class AccessLogsDataset(Dataset):
     def __init__(self, csv_path):
         """
         Method to instantiate the access logs dataset.
-        :param csv_path: The csv path of the access logs dataset
+        :param csv_path: The csv path of the access logs dataset.
         """
         # load data configuration
         config = load_config()
         data_config = config['data']
 
-        # get the dataframe
+        # get the csv dataset file
         df = pd.read_csv(csv_path)
 
         # set keys and seq_len fields
@@ -24,7 +24,7 @@ class AccessLogsDataset(Dataset):
     def __len__(self):
         """
         Method to return the length of the access logs dataset.
-        :return: The length of the access logs dataset
+        :return: The length of the access logs dataset as output.
         """
         return len(self.keys) - self.seq_len
 
@@ -32,7 +32,7 @@ class AccessLogsDataset(Dataset):
         """
         Method to return a sequence and the next value in the sequence.
         :param idx: The index of the access logs dataset
-        :return: Access logs dataset
+        :return: Access logs dataset as output.
         """
         # get the sequence (x)
         x = torch.tensor(self.keys[idx:idx + self.seq_len], dtype=torch.long)
