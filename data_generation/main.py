be83a7781498af95fa5da3e347828b9f9055923b
michelepatella <patellamichele2002@gmail.com>
2025-05-15 18:53:21 +0200

    cyclic features removed because there are no real patterns in synthetic data

--- data_generation/main.py
+++ data_generation/main.py
@@ -1,6 +1,5 @@
 import logging
 import pandas as pd
-from data_generation.cyclic_time_features_generator import _generate_cyclic_time_features
 from data_generation.requests_generator import _generate_static_requests, _generate_dynamic_requests
 from utils.config_utils import _get_config_value
 from utils.dataset_utils import _save_dataset, _get_dataset_path_type
@@ -26,14 +25,10 @@ def data_generation():
     else:
         raise ValueError(f"❌ Invalid distribution type: {distribution_type}")
 
-    # generate cyclic time features starting from timestamps
-    cyclic_time_features = _generate_cyclic_time_features(timestamps)
-
     # create dataframe
     df = pd.DataFrame(
         {
             "timestamp": timestamps,
-            **cyclic_time_features,
             "request": requests,
         }
     )
