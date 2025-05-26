6152151143faf25f62543ba4a3c16615411bdf0d
michelepatella <patellamichele2002@gmail.com>
2025-05-26 22:13:28 +0200

    some features removed

--- data_preprocessing/feature_engineering.py
+++ data_preprocessing/feature_engineering.py
@@ -17,15 +17,11 @@ def _build_temporal_features(df, time_column):
     # extract timestamp values
     timestamps = df[time_column].values
 
-    # create day fraction, delta time, and
-    # a column indicating whether peak or not as
-    # three new features
-    day_fraction = (timestamps % period) / period
+    # create new features
     delta_t = np.diff(timestamps, prepend=timestamps[0])
     hours = timestamps % period
     is_peak = ((hours >= 11) & (hours < 15)).astype(float)
 
-    df["day_fraction"] = day_fraction
     df["delta_t"] = delta_t
     df["is_peak"] = is_peak
 
@@ -63,12 +59,6 @@ def _encode_time_trigonometrically(
         # drop original time column
         df = df.drop(columns=[time_column])
 
-        # reorder columns: sin_time and cos_time first
-        cols = (['sin_time', 'cos_time'] +
-                [col for col in df.columns if
-                 col not in ['sin_time', 'cos_time']])
-        df = df[cols]
-
     except KeyError as e:
         raise RuntimeError(f"❌ Error during trigonometrical encoding: {e}.")
 
@@ -78,51 +68,16 @@ def _encode_time_trigonometrically(
     return df
 
 
-def _build_request_features(df, target_column, window_size=5):
-    """
-    Method to build features related to requests.
-    :param df: The dataframe to process.
-    :param target_column: The name of the target column.
-    :param window_size: The window size.
-    :return: The augmented dataframe.
-    """
-    # show initial message
-    info("🔄 Building request features started...")
-
-    # extract target column values
-    values = df[target_column].values
-
-    # generate last request, delta key, and moving
-    # average key as three new features
-    last_request = np.insert(values[:-1], 0, 0)
-    delta_key = values - last_request
-    moving_avg_key = pd.Series(values).rolling(
-        window=window_size,
-        min_periods=1
-    ).mean().values
-
-    df["last_request"] = last_request
-    df["delta_key"] = delta_key
-    df["moving_avg_key"] = moving_avg_key
-
-    # show successful message
-    info("🟢 Request features built.")
-
-    return df
-
-
 def build_features(
         df,
         time_column,
         target_column,
-        window_size=5
 ):
     """
     Method to orchestrate feature engineering.
     :param df: The original dataframe.
     :param time_column: The time column.
     :param target_column: The target column.
-    :param window_size: The window size.
     :return: The final dataframe.
     """
     # show initial message
@@ -131,11 +86,6 @@ def build_features(
     try:
         # build new features
         df = _build_temporal_features(df, time_column)
-        df = _build_request_features(
-            df,
-            target_column,
-            window_size
-        )
         df = _encode_time_trigonometrically(df, time_column)
 
         # reorder column s.t. target column is the last one
